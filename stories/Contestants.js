e6216989b50fb1723cc12be275a200b1c8bb4e6f
evanhosni <evankhaledhosni@gmail.com>
2022-04-11 15:48:04 -0700

    added gif backgrounds for microscope, microscope functionality complete

--- stories/Contestants.js
+++ stories/Contestants.js
@@ -7,7 +7,9 @@ import spoons from './images/spoons.png'
 import chili1 from './images/chili1.png'
 import chili2 from './images/chili2.png'
 import chili3 from './images/chili3.png'
-// import chili4 from './images/chili4.png'
+import fetal_neurons from './images/fetal_neurons.gif'
+import mitosis from './images/mitosis.gif'
+import nanobots from './images/nanobots.gif'
 
 export const createContestants = () => {
     const contestants = document.createElement('section')
@@ -16,9 +18,9 @@ export const createContestants = () => {
     contestants.appendChild(createHeader({ text: 'Contestants' }))
 
     const content = document.createElement('div')
-    content.appendChild(createCard({ imgSrc: chili1, imgAlt: 'chili', text: "Nanobot technology sure has come a long way. This synthetic chili taps into your hippocampus to retrieve all those nostalgic memories..." }))
-    content.appendChild(createCard({ imgSrc: chili2, imgAlt: 'chili', text: "Contestant #2 chose to use only the freshest, wholest of ingredients. We're talking pre-cell division stuff. Talk about organic!" }))
-    content.appendChild(createCard({ imgSrc: chili3, imgAlt: 'chili', text: "A new twist on GMOs, this chili blah blah blah. Anyways take a bite and let me know what you think." }))
+    content.appendChild(createCard({ imgSrc: chili1, imgAlt: 'chili', gifSrc: nanobots, gifAlt: 'nanobots', text: "Nanobot technology sure has come a long way. This synthetic chili taps into your hippocampus to retrieve all those nostalgic memories..." }))
+    content.appendChild(createCard({ imgSrc: chili2, imgAlt: 'chili', gifSrc: mitosis, gifAlt: 'mitosis', text: "Contestant #2 chose to use only the freshest, wholest of ingredients. We're talking pre-cell division stuff. Talk about organic!" }))
+    content.appendChild(createCard({ imgSrc: chili3, imgAlt: 'chili', gifSrc: fetal_neurons, gifAlt: 'fetal neurons', text: "A new twist on GMOs, this chili blah blah blah. Anyways take a bite and let me know what you think." }))
 
     contestants.appendChild(content)
 
--- stories/components/Card.js
+++ stories/components/Card.js
@@ -4,6 +4,8 @@ import { createParagraph } from './Paragraph';
 export const createCard = ({
     imgSrc,
     imgAlt,
+    gifSrc,
+    gifAlt,//TODO: figure out how to include alt on background elements
     text,
 }) => {
     const card = document.createElement('div')
@@ -16,76 +18,83 @@ export const createCard = ({
     const container = document.createElement('div')
     container.className = 'magnifiable'
 
-    image.addEventListener('mouseover', () => {
-        var microscopes = document.querySelectorAll('.microscope')
-        for (let i = 0; i < microscopes.length; i++) {
-            microscopes[i].remove()
-        }
-        var glass, w, h, bw;
-        let zoom = 5;
-        /*create magnifier glass:*/
-        glass = document.createElement("div");
-        glass.setAttribute("class", "microscope");
-        /*insert magnifier glass:*/
-        image.parentElement.insertBefore(glass, image);
-        /*set background properties for the magnifier glass:*/
-        glass.style.backgroundImage = "url('http://placekitten.com/2000/2000')";
-        glass.style.backgroundRepeat = "no-repeat";
-        glass.style.backgroundSize = (image.width * zoom) + "px " + (image.height * zoom) + "px";
-        bw = 3;
-        w = glass.offsetWidth / 2;
-        h = glass.offsetHeight / 2;
-        /*execute a function when someone moves the magnifier glass over the image:*/
-        glass.addEventListener("mousemove", moveMagnifier);
-        image.addEventListener("mousemove", moveMagnifier);
-        /*and also for touch screens:*/
-        glass.addEventListener("touchmove", moveMagnifier);
-        image.addEventListener("touchmove", moveMagnifier);
-        function moveMagnifier(e) {
-          var pos, x, y;
-          /*prevent any other actions that may occur when moving over the image*/
-          e.preventDefault();
-          /*get the cursor's x and y positions:*/
-          pos = getCursorPos(e);
-          x = pos.x;
-          y = pos.y;
-          /*prevent the magnifier glass from being positioned outside the image:*/
-          if (x > image.width - (w / zoom)) {x = image.width - (w / zoom);}
-          if (x < w / zoom) {x = w / zoom;}
-          if (y > image.height - (h / zoom)) {y = image.height - (h / zoom);}
-          if (y < h / zoom) {y = h / zoom;}
-          /*set the position of the magnifier glass:*/
-          glass.style.left = (x - w) + "px";
-          glass.style.top = (y - h) + "px";
-          /*display what the magnifier glass "sees":*/
-          glass.style.backgroundPosition = "-" + ((x * zoom) - w + bw) + "px -" + ((y * zoom) - h + bw) + "px";
-        }
-        function getCursorPos(e) {
-          var a, x = 0, y = 0;
-          e = e || window.event;
-          /*get the x and y positions of the image:*/
-          a = image.getBoundingClientRect();
-          /*calculate the cursor's x and y coordinates, relative to the image:*/
-          x = e.pageX - a.left;
-          y = e.pageY - a.top;
-          /*consider any page scrolling:*/
-          x = x - window.pageXOffset;
-          y = y - window.pageYOffset;
-          return {x : x, y : y};
-        }
-    })
-
-    image.addEventListener('mouseleave', (e) => { //TODO: glitchy
-        // console.log(e.target)
-        // var microscopes = document.querySelectorAll('.microscope')
-        // for (let i = 0; i < microscopes.length; i++) {
-        //     microscopes[i].remove()
-        // }
-    })
-
     container.appendChild(image)
     card.appendChild(container)
     card.appendChild(createParagraph({ text: text }));
+
+    image.addEventListener('mouseover', () => {
+        //remove old/duplicate microscopes
+        deleteMicroscope()
+
+        var glass, w, h, borderWidth;
+        var zoom = 2;
+        
+        //create microscope
+        glass = document.createElement('div');
+        glass.className = 'microscope';
+        image.parentElement.insertBefore(glass, image);
+
+        //set microscope background to gifSrc
+        glass.style.backgroundImage = `url('${gifSrc}')`;
+        glass.style.backgroundRepeat = 'no-repeat';
+        glass.style.backgroundSize = (image.width * zoom) + 'px ' + (image.height * zoom) + 'px';
+        borderWidth = 3;
+        w = glass.offsetWidth / 2;
+        h = glass.offsetHeight / 2;
+
+        //call moveMicroscope on mousemove
+        glass.addEventListener('mousemove', moveMicroscope);
+        image.addEventListener('mousemove', moveMicroscope);
+        //call moveMicroscope on touchmove (for touchscreen compatability)
+        glass.addEventListener('touchmove', moveMicroscope);
+        image.addEventListener('touchmove', moveMicroscope);
+
+        function moveMicroscope(e) {
+            e.preventDefault();
+
+            var x, y;
+
+            //get cursor coordinates
+            x = getCursorPos(e).x;
+            y = getCursorPos(e).y;
+
+            //prevent microscope from leaving magnifiable image bounds
+            if (x > image.width - (w / zoom)) {x = image.width - (w / zoom);}
+            if (x < w / zoom) {x = w / zoom;}
+            if (y > image.height - (h / zoom)) {y = image.height - (h / zoom);}
+            if (y < h / zoom) {y = h / zoom;}
+
+            //set microscope position
+            glass.style.left = (x - w) + 'px';
+            glass.style.top = (y - h) + 'px';
+
+            //display background image in microscope
+            glass.style.backgroundPosition = '-' + ((x * zoom) - w + borderWidth) + 'px -' + ((y * zoom) - h + borderWidth) + 'px';
+        }
+
+        function getCursorPos(e) {
+            var a, x = 0, y = 0;
+            e = e || window.event;
+            a = image.getBoundingClientRect();
+            x = e.pageX - a.left;
+            y = e.pageY - a.top;
+            x = x - window.pageXOffset;
+            y = y - window.pageYOffset;
+            return {x : x, y : y};
+        }
+
+        function deleteMicroscope() {
+            var microscopes = document.querySelectorAll('.microscope')
+            for (let i = 0; i < microscopes.length; i++) {
+                microscopes[i].remove()
+            }
+        }
+
+        //delete microscope when cursor leaves magnifiable area
+        glass.addEventListener('mouseleave', () => {
+            deleteMicroscope()
+        })
+    })
 
     return card;
 }
